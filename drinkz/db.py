10037555d99cabd108ac19efddc2a447685a0336
Ryan Tempas <tempasry@gmail.com>
2013-02-12 15:37:42 -0500

    Dat Homework3, brooooo

--- drinkz/db.py
+++ drinkz/db.py
@@ -3,14 +3,16 @@ Database functionality for drinkz information.
 """
 
 # private singleton variables at module level
-_bottle_types_db = []
-_inventory_db = []
+_inventory_db = {}
+_bottle_types_db = set()
+
+
 
 def _reset_db():
     "A method only to be used during testing -- toss the existing db info."
     global _bottle_types_db, _inventory_db
-    _bottle_types_db = []
-    _inventory_db = []
+    _bottle_types_db = set()
+    _inventory_db = {}
 
 # exceptions in Python inherit from Exception and generally don't need to
 # override any methods.
@@ -19,7 +21,7 @@ class LiquorMissing(Exception):
 
 def add_bottle_type(mfg, liquor, typ):
     "Add the given bottle type into the drinkz database."
-    _bottle_types_db.append((mfg, liquor, typ))
+    _bottle_types_db.add((mfg, liquor, typ))
 
 def _check_bottle_type_exists(mfg, liquor):
     for (m, l, _) in _bottle_types_db:
@@ -33,12 +35,18 @@ def add_to_inventory(mfg, liquor, amount):
     if not _check_bottle_type_exists(mfg, liquor):
         err = "Missing liquor: manufacturer '%s', name '%s'" % (mfg, liquor)
         raise LiquorMissing(err)
+    
+    addThis = (mfg, liquor)
 
     # just add it to the inventory database as a tuple, for now.
-    _inventory_db.append((mfg, liquor, amount))
+    if addThis in _inventory_db:
+        _inventory_db[addThis].append(amount)
+    else:
+        _inventory_db[addThis] = []
+        _inventory_db[addThis].append(amount)
 
 def check_inventory(mfg, liquor):
-    for (m, l, _) in _inventory_db:
+    for (m, l) in _inventory_db.keys():
         if mfg == m and liquor == l:
             return True
         
@@ -50,11 +58,8 @@ def get_liquor_amount(mfg, liquor):
     #conversion rate: ml to Fl Oz
     ozToMl =  29.5735296
     
-    amounts = []
-    for (m, l, amount) in _inventory_db:
-        if mfg == m and liquor == l:
-            amounts.append(amount)
-    
+    amounts = _inventory_db[(mfg, liquor)]
+
     total = 0
     for amount in amounts:
         if 'ml' in amount:
@@ -75,5 +80,5 @@ def get_liquor_amount(mfg, liquor):
 
 def get_liquor_inventory():
     "Retrieve all liquor types in inventory, in tuple form: (mfg, liquor)."
-    for (m, l, _) in _inventory_db:
+    for (m, l) in _inventory_db.keys():
         yield m, l
